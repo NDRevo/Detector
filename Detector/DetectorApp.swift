d20337012ad09bc8c3107a5f1398e4e8039cb6f3
Noe Duran <durann031@gmail.com>
2022-05-25 10:35:51 -0400

    Initial Commit

---
+++ Detector/DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectorApp.swift
+//  Detector
+//
+//  Created by Noé Duran on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
